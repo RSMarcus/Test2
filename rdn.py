5ed947ef38750ec5b4ca6a0765024b6fcb5c63d9
RSMarcus <mamarcus@hotmail.fr>
2022-02-26 11:57:06 +1100

    Test

---
+++ rdn.py
@@ -0,0 +1,29 @@
+class Account:
+    def __init__(self):
+        self.accountNumber
+        self.balance
+        self.annualIntRate
+        self.dateCreated
+
+    def deposit(self):
+
+
+
+    def withdraw(self):
+
+
+
+class TestAccount:
+
+
+
+
+class SavingsAccount:
+
+
+
+
+class CheckingAccount:
+
+
+
